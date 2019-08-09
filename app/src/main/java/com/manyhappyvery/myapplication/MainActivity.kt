5af591b981c66a3d02e5bbf9d895d2c92a595ccc
Krizhevskii, Pavel <Pavel.Krizhevskii@harman.com>
2019-08-09 16:41:23 +0200

    second

--- app/src/main/java/com/manyhappyvery/myapplication/MainActivity.kt
+++ app/src/main/java/com/manyhappyvery/myapplication/MainActivity.kt
@@ -2,11 +2,14 @@ package com.manyhappyvery.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.media.MediaPlayer
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView1.text = "nice day"
     }
 }
