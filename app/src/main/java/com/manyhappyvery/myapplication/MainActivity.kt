42b1fc626481d6bf3a590d560d9bb9ce0beaaabb
Krizhevskii, Pavel <Pavel.Krizhevskii@harman.com>
2019-08-09 17:01:01 +0200

    forth

--- app/src/main/java/com/manyhappyvery/myapplication/MainActivity.kt
+++ app/src/main/java/com/manyhappyvery/myapplication/MainActivity.kt
@@ -2,11 +2,13 @@ package com.manyhappyvery.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Header01.text = "Simple test in BT paring"
     }
 }
