efb840aee67a5ba883fe3fabef2459815ba8d2a2
Krizhevskii, Pavel <Pavel.Krizhevskii@harman.com>
2019-08-09 19:52:32 +0200

    scroller

--- app/src/main/java/com/manyhappyvery/myapplication/MainActivity.kt
+++ app/src/main/java/com/manyhappyvery/myapplication/MainActivity.kt
@@ -6,9 +6,19 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+var a = 1
+var b = "aaa"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Header01.text = "Simple test in BT paring"
+
+        Header01.text = "Simple test in BT paring0"  + "\n" + "aaaaa" + "\n" + "bbbbb  " + "\n" + "aaaaa" + "\n" + "bbbbb  " + "\n" + "aaaaa" + "\n" + "bbbbb  " + "\n" + "aaaaa" + "\n" + "bbbbb  " + "\n" + "aaaaa" + "\n" + "bbbbb  " + "\n" + "aaaaa" + "\n" + "bbbbb  " + "\n" + "aaaaa" + "\n" + "bbbbb  " + "\n" + "aaaaa" + "\n" + "bbbbb  " + "\n" + "aaaaa" + "\n" + "bbbbb  " + "\n" + "aaaaa" + "\n" + "bbbbb  " + "\n" + "aaaaa" + "\n" + "bbbbb  "
+        Header01.isVerticalScrollBarEnabled = true
+        Header01.scrollBarSize = 5
+        ;
+        a = Header01.scrollY
+        b = a.toString()
+        body1.text = b
+
     }
 }
