b4aa67d64597013b3d1fc7c53121fc4bd8497839
Krizhevskii, Pavel <Pavel.Krizhevskii@harman.com>
2019-08-09 21:29:46 +0200

    03Btstg

--- app/src/main/java/com/manyhappyvery/myapplication/MainActivity.kt
+++ app/src/main/java/com/manyhappyvery/myapplication/MainActivity.kt
@@ -1,12 +1,42 @@
 package com.manyhappyvery.myapplication
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var m_bluetoothAdapter: BluetoothAdapter?
+    lateinit var m_pairedDevices: Set<BluetoothDevice>
+    val REQUEST_ENABLE_BLUETOOTH  = 1
+
+
+    companion object {
+        val EXTRA_ADDRESS: String = "Device_adress"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //TheLog.text = "avvvvv" + "\n" + "bmnjjjjjjjj" + "\n" + "ahhhhhhhh" + "\n" + "b90999999999" + "\n333333333" + "a665555665656566556" + "\n" + "booooiiiiii" + "\n" + "a" + "\n" + "b" + "\n" + "a" + "\n" + "b" + "\n"
+
+        m_bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+
+        if (m_bluetoothAdapter == null )
+        {
+
+        }
+    private fun pairedDeviceList () {
+
+    }
+
+
+        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+
+
     }
 }
